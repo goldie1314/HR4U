e3823c013ad96d014ac0d84e10b493df1059477c
goldie1314 <hr4you_bg@outlook.com>
2025-05-10 16:11:09 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
   if (req.method !== 'POST') {

214dfea35b5b8ae0e3129c7e6fc6000615301e39
goldie1314 <hr4you_bg@outlook.com>
2025-05-10 17:39:40 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,8 +1,1 @@
-function toggleChat() {
-  var chatWindow = document.getElementById("chatWindow");
-  if (chatWindow.style.display === "none" || chatWindow.style.display === "") {
-    chatWindow.style.display = "block";
-  } else {
-    chatWindow.style.display = "none";
-  }
-}
+
